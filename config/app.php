d8397a2a89ee570453a2135dd2ee79b0ec0c3962
Ram Dedomo <ramdedomo@gmail.com>
2023-01-22 09:43:32 +0800

    removed krlove plugin

--- config/app.php
+++ config/app.php
@@ -159,7 +159,7 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
-        Krlove\EloquentModelGenerator\Provider\GeneratorServiceProvider::class,
+        //Krlove\EloquentModelGenerator\Provider\GeneratorServiceProvider::class,
         Kreait\Laravel\Firebase\ServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
