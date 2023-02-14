2b92f8bd548a106e05d7fc95948485ac8a6d62ef
Ram Dedomo <ramdedomo@gmail.com>
2023-02-14 18:00:00 +0800

    removed unnecessary files

--- config/app.php
+++ config/app.php
@@ -160,7 +160,6 @@ return [
          * Laravel Framework Service Providers...
          */
 
-        Kreait\Laravel\Firebase\ServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
